c3375780efddf91bf11bcee7f6e9f240f830dbc2
Scynes <dustin.greyfield@gmail.com>
2023-05-21 11:52:48 -0400

    wip/complete: finished navigation bar formatting and active toggle

--- src/components/navigation/NavigationBar.js
+++ src/components/navigation/NavigationBar.js
@@ -1,6 +1,19 @@
+import { NavLink } from "react-router-dom";
+
 const NavigationBar = () => {
     return (
-        <h1>Hello</h1>
+        <section className="navigation-container">
+            <nav className="navigation">
+                <NavLink to={'/'}>
+                    <h1 className="nav-logo">navigatedu</h1>
+                </NavLink>
+                <NavLink to={'/learn'} className={({ isActive }) => (isActive ? 'active nav-item' : 'nav-item')}>Learn</NavLink>
+                <NavLink to={'/glossary'} className={({ isActive }) => (isActive ? 'active nav-item' : 'nav-item')}>Glossary</NavLink>
+                <NavLink to={'/about'} className={({ isActive }) => (isActive ? 'active nav-item' : 'nav-item')}>About</NavLink>
+                <NavLink to={'/contact'} className={({ isActive }) => (isActive ? 'active nav-item' : 'nav-item')}>Contact</NavLink>
+            </nav>
+            <input type="text" name="" id="" />
+        </section>
     );
 }
 
